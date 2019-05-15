a7f6e9c688a53f8f489f2855269250ee8cbb0cee
Decemmber <eu.krimp29@gmail.com>
2019-05-16 02:12:16 +0700

    responsive done

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-    $('.multiple-items').slick({
-        lazyLoad: 'ondemand',
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        arrows: false,
-        dots: true,
-        dotsClass: 'dots-style'
-      });
-})
+$(document).ready(function(){$(".multiple-items").slick({lazyLoad:"ondemand",infinite:!0,slidesToShow:4,slidesToScroll:2,arrows:!1,dots:!0,dotsClass:"dots-style",responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:2}},{breakpoint:767,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:479,settings:{slidesToShow:1,slidesToScroll:1}}]}),$(window).scroll(function(){0!=$(this).scrollTop()?$("#toTop").fadeIn():$("#toTop").fadeOut()}),$("#toTop").click(function(){$("body,html").animate({scrollTop:0},800)})});
